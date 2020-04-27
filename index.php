cba8119b35d91df27bd7e47f269805a54aad6559
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-04-27 23:39:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 	    <link rel="stylesheet" type="text/css" href="css/style.css" />
          <link rel="stylesheet" type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
 	    <script src="js/jquery.min.js"></script>
-		<script src="js/jquery.form.min.js"></script>
+        <script src="js/jquery.form.min.js"></script>
 	    <script language="JavaScript" src="js/javascript.js"></script>
 
 	</head>
 <body>
 <center>
-	    <div class="main_section">
+	    <div class="main_section" style="height: auto;">
 	    <div class="nav_bar">
 	    	<a href=""><img align="left" src="images/logo.png" class="logo" /></a>
         <ul class="nav_bar_menu">
@@ -23,61 +23,19 @@
         	<a href="about_us"><li>ABOUT US</li></a>
         </ul>
 	    </div>
-	    <div class="main_content">
-	    	<br />
-	    	<p>XForUp | Upload your files in a simple way any where and any time</p>
-	        <br />
-	        <a class="upload_btn_main" href="upload">UPLOAD FILES</a>
 	    </div>
-	    </div>
-	    <div class="why_us">
-	    	<div class="why_us_con">
-	    		<img src="images/ss_07.png" />
-	    	</div>
-	    	<div class="why_us_con">
-	    		<h1>Why Us?</h1>
-	    		<p>
-	    		Because we keep your files in safe<br />
-	    		and you can upload your files even 2GB<br />
-	    		for free without register to our website<br />
-	    		and we supported alot of formats like<br />
-	    		images formats and documents and videos and also musics<br />
-	    		and more..
-	    		</p>
-	    	</div>
-	    </div>
-	    <div class="features_section">
-	    	<div class="features_info">
-	    		<span class="fa fa-pencil-square-o" style="color: gray;font-size: 45px;"></span>
-	    		<h2>Free and simple</h2>
-	    		<p>
-	    			Upload your files for free without<br>
-	    			register and in a simple way,<br>
-	    			Any time and any where with XForUp<br>
-	    			Maximum size to upload files 2GB free<br>
-	    		</p>
-	    	</div>
-	    	<div class="features_info">
-	    		<span class="fa fa-file-text" style="color: gray;font-size: 45px;"></span>
-	    		<h2>Supported Formats</h2>
-	    		<p>
-	    			We are support alot of formats<br>
-	    			like images files png , jpg...<br>
-	    			and document formats like txt , rtf<br>
-	    			and more formats you can find it <a href="#">here</a><br>
-	    		</p>
-	    	</div>
-	    	<div class="features_info">
-	    		<span class="fa fa-envelope-o" style="color: gray;font-size: 45px;"></span>
-	    		<h2>Contact us</h2>
-	    		<p>
-	    			If you want to contact us :<br>
-	    			phone : +9647821704671 - Zain IQ<br>
-	    			E-mail : munafaqeelmahdi@gmail.com<br>
-	    			Facebook : fb.com/munaf.aqeel.m<br>
-	    		</p>
-	    	</div>
-	    </div>
+	    <div class="upload_section" id="upload_f_section">
+	    	<form action="include/up_func.php" id="uploadForm" method="post" enctype="multipart/form-data">
+	    		<h1 style="color: #1e81d0;text-shadow: 5px 5px 1px #e8e8e8;"><span class="fa fa-cloud-download"></span> Upload Your Files</h1>
+    	<div class="upload_box">
+    	<input class="upload_feild" type="file" name="fileToUpload"/>
+    	<p><input class="upload_btn" type="submit" value="Upload" name="uploadFile"></p>
+        <p id="uploading" style="display:none;"><span style="padding: 7px 15px;background: #fff;border-radius: 3px;color: #2196f3;" id="uploadingTXT">Uploading...</span></p>	
+    	</div>
+        </form>
+        <div id="uploaded_result">
+        </div>
+    	</div>
     	<div class="footer">
     		<span class="social_networks" style="float: right">
     			<a href="#"><img class="fb" /></a>
@@ -91,5 +49,27 @@
 </form>
 	    </div>
 </center>
+<script>
+$(document).ready(function(){
+$("#uploadForm").on('submit',function(e){
+e.preventDefault();
+$(this).ajaxSubmit({
+beforeSend:function(){
+$("#uploadingTXT").html("Wait..");
+$("#uploading").show();
+$(".upload_btn").hide();
+},
+uploadProgress:function(event,position,total,percentCompelete){
+$("#uploadingTXT").html("uploading... [ "+percentCompelete+"% ]");
+},
+success:function(data){
+$("#uploadingTXT").html("Done");
+$("#uploading").hide();
+$(".upload_btn").show();
+$("#uploaded_result").html(data);
+}});
+});
+});
+</script>
 </body>
 </html>
